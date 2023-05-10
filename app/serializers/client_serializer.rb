8031564f810d245e76353e0fd584cfb596aa9545
Charles Swaleh <charlswall@live.com>
2023-05-10 13:51:48 +0300

    Bonus: On client show route, send response that includes total amount for all membership for that client

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,3 +1,7 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age
+  attributes :id, :name, :age, :total_memberships_amount
+
+  def total_memberships_amount
+    self.object.memberships.map {|membership| membership.charge }.sum
+  end
 end
