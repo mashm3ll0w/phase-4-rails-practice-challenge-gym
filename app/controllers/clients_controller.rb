84bd05d964ad6dc888cea0bd6ee460feddd56e78
Charles Swaleh <charlswall@live.com>
2023-05-10 13:44:17 +0300

    Added the show route for the Client model

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,18 @@
 class ClientsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
+
+  def show
+    client = find_client
+    render json: client, status: :ok
+  end
+
+  private
+
+  def find_client
+    Client.find(params[:id])
+  end
+
+  def not_found_error
+    render json: {error: "Client not found"}, status: :not_found
+  end
 end
