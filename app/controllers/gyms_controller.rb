07a3e7652b44aeb78a34c057d237885475be3d01
Charles Swaleh <charlswall@live.com>
2023-05-10 14:10:10 +0300

    Bonus: Create index and update routes for Gym

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,8 +1,20 @@
 class GymsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_params
+  wrap_parameters format: []
+
+  def index
+    render json: Gym.all, status: :ok
+  end
 
   def show
     gym = find_gym
+    render json: gym, status: :ok
+  end
+
+  def update
+    gym = find_gym
+    gym.update!(gym_params)
     render json: gym, status: :ok
   end
 
@@ -20,6 +32,14 @@ class GymsController < ApplicationController
 
   def not_found_error
     render json: {error: "Gym not found"}, status: :not_found
+  end
+
+  def gym_params
+    params.permit(:name, :address)
+  end
+
+  def invalid_params(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
 end
