0cc141af2bd9bca515f155df60f0444c11c907e6
Charles Swaleh <charlswall@live.com>
2023-05-10 13:39:16 +0300

    Added the show and destroy routes for the Gym model and the associations

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,2 +1,25 @@
 class GymsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
+
+  def show
+    gym = find_gym
+    render json: gym, status: :ok
+  end
+
+  def destroy
+    gym = find_gym
+    gym.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_gym
+    Gym.find(params[:id])
+  end
+
+  def not_found_error
+    render json: {error: "Gym not found"}, status: :not_found
+  end
+
 end
